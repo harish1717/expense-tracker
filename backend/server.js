3dfc126f6a9232a2d326c33358003f5f76539642
harish1717 <harisharumugam0717@gmail.com>
2026-09-25 20:03:52 +0530

    Auth is done

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', require('./routes/authRoutes'));
 
 app.get('/', (req, res) => {
 	res.send('Smart Expense Tracker API is running');
 });
+
 
 mongoose.connect(process.env.MONGO_URI)
 	.then(() => console.log('✅ MongoDB connected'))
